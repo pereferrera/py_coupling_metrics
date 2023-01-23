9a5cdfbacb6c09f8a9e3399ee439b6a15b9453a3
Pere Ferrera <p.ferrera@mediaire.de>
2023-01-23 17:23:21 +0100

    Added support for different levels of grouping

--- coupling_metrics/coupling.py
+++ coupling_metrics/coupling.py
@@ -9,11 +9,6 @@ from typing import Dict, List
 logger = logging.getLogger(__name__)
 
 
-"""
-TODO Support different levels of grouping (packages/sub-packages)
-"""
-
-
 @dataclass
 class CouplingMetrics:
 
@@ -58,7 +53,17 @@ class CouplingMetrics:
         )
 
 
-def generate_metrics(project_root: str, package_name: str) -> CouplingMetrics:
+def adjust_module_name_to_level(module: str, level: int):
+    if level < 1:
+        # full module name
+        return module
+    splitted = module.split('.')
+    return '.'.join(splitted[:min(len(splitted),
+                                  level)])
+
+
+def generate_metrics(project_root: str, package_name: str,
+                     level: int = 0) -> CouplingMetrics:
     project_root = os.path.abspath(project_root)
     if not project_root.endswith('/'):
         project_root += '/'
@@ -88,9 +93,8 @@ def generate_metrics(project_root: str, package_name: str) -> CouplingMetrics:
         return any(isinstance(n, ast.FunctionDef)
                    and has_abstract_method(n) for n in node.body)
 
-    def parse_import_node(node, dependencies):
+    def parse_import_node(node, dependencies, module_name, level):
         for alias in node.names:
-            # TODO what would happen with a * alias?
             to_corresponding_module = (
                 '/'.join(f"{node.module}.{alias.name}"
                          .split('.'))
@@ -107,66 +111,87 @@ def generate_metrics(project_root: str, package_name: str) -> CouplingMetrics:
 
             if not to_corresponding_module:
                 logger.error(f'{node.module}.{alias.name} '
-                             'cannot be traced back a python file')
+                             'cannot be traced back to a module file!')
                 continue
 
             to_corresponding_module = '.'.join(
                 '.'.join(to_corresponding_module.split('/')).split('.')[:-1]
             )
             logger.info(f' - imports {to_corresponding_module}')
-            dependencies.add(to_corresponding_module)
+
+            dependency = adjust_module_name_to_level(to_corresponding_module,
+                                                     level)
+            if module_name != dependency:
+                dependencies.add(dependency)
+
+    afferent_set = {}
+    efferent_set = {}
+    uml_diagram_set = set()
 
     for root, _, files in os.walk(os.path.join(project_root, package_name),
                                   topdown=False):
         for name in files:
-            if name.endswith('.py'):
-                file_path = os.path.join(root, name)
-                # super hacky?
-                module_name = (
-                    file_path.replace(project_root, '')
-                    .replace('/', '.')
-                    .replace('.py', '')
+            if not name.endswith('.py'):
+                continue
+
+            file_path = os.path.join(root, name)
+            # super hacky?
+            module_name = adjust_module_name_to_level(
+                file_path.replace(project_root, '')
+                .replace('/', '.')
+                .replace('.py', ''),
+                level=level
+            )
+            logger.info(f'Parsing {module_name}')
+
+            abstract = 0
+            concrete = 0
+
+            dependencies = set()
+
+            with open(file_path, "r") as file:
+                code = file.read()
+                module = ast.parse(code)
+
+                for node in module.body:
+                    if isinstance(node, ast.FunctionDef):
+                        concrete += 1
+                    elif isinstance(node, ast.ClassDef):
+                        abstract += (
+                            1 if is_abstract_class(node) else 0
+                        )
+                    elif isinstance(node, ast.ImportFrom):
+                        if not node.module.startswith(package_name):
+                            continue
+                        parse_import_node(node, dependencies, module_name,
+                                          level)
+
+            for dependency in dependencies:
+                # draw a UML line in PlantUML format
+                to_ = '.'.join(f"{dependency}".split('.')[1:])
+                from_ = '.'.join(module_name.split('.')[1:])
+                uml_diagram_set.add((from_, to_))
+                afferent_set[dependency] =\
+                    afferent_set.get(dependency, set()).union(
+                        set([module_name]))
+
+            efferent_set[module_name] =\
+                efferent_set.get(module_name, set()).union(dependencies)
+
+            if abstract + concrete > 0:
+                metrics.abstractness[module_name] = (
+                    abstract / (abstract + concrete)
                 )
-                logger.info(f'Parsing {module_name}')
-                depends_on = 0
-                abstract = 0
-                concrete = 0
 
-                dependencies = set()
+    for from_, to_ in uml_diagram_set:
+        arrow_direction = random.choice(['', 'up.', 'down.'])
+        metrics.uml_diagram.append(
+            f"  [{from_}] .{arrow_direction}> [{to_}]")
 
-                with open(file_path, "r") as file:
-                    code = file.read()
-                    module = ast.parse(code)
-
-                    for node in module.body:
-                        if isinstance(node, ast.FunctionDef):
-                            concrete += 1
-                        elif isinstance(node, ast.ClassDef):
-                            abstract += (
-                                1 if is_abstract_class(node) else 0
-                            )
-                        elif isinstance(node, ast.ImportFrom):
-                            if not node.module.startswith(package_name):
-                                continue
-                            parse_import_node(node, dependencies)
-
-                for dependency in dependencies:
-                    # draw a UML line in PlantUML format
-                    to_ = '.'.join(f"{dependency}".split('.')[1:])
-                    arrow_direction = random.choice(['', 'up.', 'down.'])
-                    from_ = '.'.join(module_name.split('.')[1:])
-                    metrics.uml_diagram.append(
-                        f"  [{from_}] .{arrow_direction}> [{to_}]")
-                    metrics.afferent_count[dependency] =\
-                        metrics.afferent_count.get(dependency, 0) + 1
-                    depends_on += 1
-
-                metrics.efferent_count[module_name] = depends_on
-
-                if abstract + concrete > 0:
-                    metrics.abstractness[module_name] = (
-                        abstract / (abstract + concrete)
-                    )
+    for module, deps in efferent_set.items():
+        metrics.efferent_count[module] = len(deps)
+    for module, deps in afferent_set.items():
+        metrics.afferent_count[module] = len(deps)
 
     metrics.uml_diagram.append("}")
     metrics.uml_diagram.append("@enduml")
--- tests/coupling_metrics/test_coupling.py
+++ tests/coupling_metrics/test_coupling.py
@@ -2,6 +2,7 @@ import unittest
 import random
 
 from coupling_metrics import coupling
+from coupling_metrics.coupling import adjust_module_name_to_level
 
 """
 TODO add example project of paper "Software Instability Analysis ..."
@@ -19,3 +20,17 @@ class TestCouplingMetrics(unittest.TestCase):
         # TODO add assertions
         for line in metrics.uml_diagram:
             print(line)
+
+    def test_adjust_module_name_to_level(self):
+        self.assertEqual('foo.bar',
+                         adjust_module_name_to_level('foo.bar', -1))
+        self.assertEqual('foo',
+                         adjust_module_name_to_level('foo', 1))
+        self.assertEqual('foo',
+                         adjust_module_name_to_level('foo.bar.bor', 1))
+        self.assertEqual('foo.bar',
+                         adjust_module_name_to_level('foo.bar.bor', 2))
+        self.assertEqual('foo.bar.bor',
+                         adjust_module_name_to_level('foo.bar.bor', 3))
+        self.assertEqual('foo.bar.bor',
+                         adjust_module_name_to_level('foo.bar.bor', 0))
